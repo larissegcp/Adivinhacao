f28d1acc10e42bc4264d5e817bfe9b8ca954c5aa
Larisse Pinto <larissegpinto@gmail.com>
2020-03-30 21:29:08 -0300

    Uso do FOR  para definir o range de tentativas do usuário

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,24 +3,26 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
 
-chute_str = input("Digite o seu número: ")
 
-print("Você digitou", chute_str)
+for rodada in range(1, total_de_tentativas + 1) :
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou  = chute == numero_secreto
+    maior    = chute > numero_secreto
+    menor    = chute < numero_secreto
 
-acertou  = chute == numero_secreto
-maior    = chute > numero_secreto
-menor    = chute < numero_secreto
-
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você errou! Seu chute foi menor do que o número secreto.")
+    if(acertou):
+            print("Você acertou!")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+             print("Você errou! Seu chute foi menor do que o número secreto.")
 
 
 print("Fim do jogo ): ")
