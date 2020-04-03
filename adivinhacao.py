1e534509721deff80ed58e2ec7bf0c84d65c9f9b
Larisse Pinto <larissegpinto@gmail.com>
2020-04-03 10:19:12 -0300

    Adição de níveis fácil, médio e difícil

--- adivinhacao.py
+++ adivinhacao.py
@@ -1,10 +1,26 @@
+import random
+
 print("*********************************")
 print("Bem vindo ao jogo de adivinhação!")
 print("*********************************")
 
-numero_secreto = 42
-total_de_tentativas = 3
+numero_secreto = random.randrange(1, 101)
+total_de_tentativas = 0
 
+print("Qual o nível de dificuldade?")
+print("(1) Fácil   (2) Médio  (3) Difícil")
+
+nivel = int(input("Defina o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+
+elif(nivel == 2):
+    total_de_tentativas = 10
+
+else:
+    (nivel == 3)
+    total_de_tentativas = 5
 
 for rodada in range(1, total_de_tentativas + 1) :
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
