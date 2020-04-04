9b8f7e979f155890d325466db934c26028e2e44e
Larisse Pinto <larissegpinto@gmail.com>
2020-04-04 11:57:03 -0300

    Iniciando o projeto de um jogo de forca utilizando Python

---
+++ forca.py
@@ -0,0 +1,18 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo de Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    #enquanto(True)
+    while(not enforcou and not acertou):
+        print("jogando...")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
