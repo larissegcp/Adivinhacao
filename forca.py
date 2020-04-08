c4808001d68f8069788240e25352afd840023026
Larisse Pinto <larissegpinto@gmail.com>
2020-04-08 10:28:25 -0300

    Inserção de listas

--- forca.py
+++ forca.py
@@ -4,13 +4,23 @@ def jogar():
     print("*********************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
 
     enforcou = False
     acertou = False
 
-    #enquanto(True)
     while(not enforcou and not acertou):
-        print("jogando...")
+
+        chute = input("Qual letra?")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                letras_acertadas[index] = letra
+            index = index + 1
+
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
